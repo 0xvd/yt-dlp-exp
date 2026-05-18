c9778638a767bca9b4e9f465291f4ece5970cfad
0xvd <199783523+0xvd@users.noreply.github.com>
2026-05-18 14:28:16 +0530

    ytdlp PR 16268 - [ie/joysound:cafe] Add extractor
    Merge PR 16268 - [ie/joysound:cafe] Add extractor
    
    Co-authored-by: doe1080 <98906116+doe1080@users.noreply.github.com>

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -936,6 +936,7 @@ from .jiosaavn import (
 )
 from .joj import JojIE
 from .jove import JoveIE
+from .joysound import JoySoundCafeIE
 from .jstream import JStreamIE
 from .jtbc import (
     JTBCIE,
