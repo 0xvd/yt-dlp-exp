bf572ec31845689e424a5af19856340a6e6c12b1
0xvd <199783523+0xvd@users.noreply.github.com>
2026-05-18 14:24:18 +0530

    ytdlp PR 13470 - [ie/rambler] Add Extractors
    Merge PR 13470 - [ie/rambler] Add Extractors
    
    Co-authored-by: doe1080 <98906116+doe1080@users.noreply.github.com>

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1670,6 +1670,7 @@ from .rai import (
     RaiPlaySoundPlaylistIE,
     RaiSudtirolIE,
 )
+from .rambler import RamblerIE
 from .raywenderlich import (
     RayWenderlichCourseIE,
     RayWenderlichIE,
