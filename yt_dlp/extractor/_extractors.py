81095f6d13f4c9538b462fd65c2e7459a7976dd6
0xvd <199783523+0xvd@users.noreply.github.com>
2026-05-18 14:20:55 +0530

    ytdlp PR 11027 - [ie/XVideosUser] Add extractor
    Merge PR 11027 - [ie/XVideosUser] Add extractor
    
    Co-authored-by: pzhlkj6612 <29089388+pzhlkj6612@users.noreply.github.com>

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2514,6 +2514,7 @@ from .xstream import XstreamIE
 from .xvideos import (
     XVideosIE,
     XVideosQuickiesIE,
+    XVideosUserIE,
 )
 from .xxxymovies import XXXYMoviesIE
 from .yahoo import (
